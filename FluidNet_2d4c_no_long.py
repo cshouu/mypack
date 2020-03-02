7f360ae37559d903d25ba7eee081a178169dbd0f
cshouu <cshouu@gmail.com>
2020-03-02 17:10:01 +0800

    The first commit

--- FluidNet_2d4c_no_long.py
+++ FluidNet_2d4c_no_long.py
@@ -26,7 +26,7 @@ def readbin(filename):
 
 
 class VPGDdataset(Dataset):
-	def __init__(self,isTrainset=True):
+	def __init__(self, isTrainset = True):
 		if isTrainset:
 			self.dataset = 'tr'
 		else:
@@ -35,50 +35,84 @@ class VPGDdataset(Dataset):
 	def __len__(self):
 		return 64*320
 
-	def __getitem__(self,idx):
+	def __getitem__(self, idx):
 		idir = idx // 64
 		ifile = (idx % 64) * 4
-		th_vx,th_px,th_gx,th_dx = readbin('./data/%s/%06d/%06d_divergent.bin' % (self.dataset,idir,ifile))
-		th_v,th_p,_1,_2 = readbin('./data/%s/%06d/%06d.bin' % (self.dataset,idir,ifile))
+		th_vx,th_px,th_gx,th_dx = readbin('D:/fluid/%s/%06d/%06d_divergent.bin' % (self.dataset,idir,ifile))
+		th_v,th_p,_1,_2 = readbin('D:/fluid/%s/%06d/%06d.bin' % (self.dataset,idir,ifile))
 		# we may or may not concatenate the input tensor with the input pressure
-		th_net_input = torch.cat([torch.unsqueeze(th_vx[:,:,0],0), torch.unsqueeze(th_vx[:,:,1],0), torch.unsqueeze(th_px,0), torch.unsqueeze(th_gx.float(),0)])
+		th_net_input = torch.cat([torch.unsqueeze(th_px,0), torch.unsqueeze(th_vx[:,:,0],0), torch.unsqueeze(th_vx[:,:,1],0), torch.unsqueeze(th_gx.float(),0)])
 		return {'th_net_input':th_net_input, 'th_vx':th_vx,
 'th_gx':th_gx, 'th_dx':th_dx, 'th_v':th_v, 'th_p':th_p}
 
 
 class PressureNet(nn.Module):
-	def __init__(self):
+	def __init__(self, modelType='default'):
 		super(PressureNet,self).__init__()
-		self.conv1 = nn.Conv2d(in_channels=4,out_channels=16,kernel_size=(3,3),padding=(1,1))
-		self.conv2 = nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=(1,1))
-		self.conv3 = nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=(1,1))
-		self.conv4 = nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=(1,1))
-		self.conv5 = nn.Conv2d(in_channels=16,out_channels=1,kernel_size=(1,1),padding=(0,0))
+		self.modelType = modelType
+		if self.modelType == 'default':
+			self.conv1 = nn.Conv2d(in_channels=4,out_channels=16,kernel_size=(3,3),padding=(1,1))
+			self.conv2 = nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=(1,1))
+			self.conv3 = nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=(1,1))
+			self.conv4 = nn.Conv2d(in_channels=16,out_channels=16,kernel_size=(3,3),padding=(1,1))
+			self.conv5 = nn.Conv2d(in_channels=16,out_channels=1,kernel_size=(1,1),padding=(0,0))
+		if self.modelType == 'tog':
+			self.conv1 = nn.Conv2d(in_channels=4,out_channels=16,kernel_size=(5,5),padding=(2,2))
+			self.pool1 = nn.MaxPool2d(kernel_size=2)
+			self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5, 5), padding=(2, 2))
+			self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(5, 5), padding=(2, 2))
+			self.conv4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5, 5), padding=(2, 2))
+			self.conv5 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(1, 1), padding=(0, 0))
+			self.conv6 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(1, 1), padding=(0, 0))
+			self.convTrans7 = nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=(3, 3), stride=(2, 2), padding=(1,1), output_padding=(1,1))
+		if self.modelType == 'yang':
+			self.conv1 = nn.Conv2d(in_channels=4, out_channels=6, kernel_size=(3, 3), padding=(1, 1))
+			self.conv2 = nn.Conv2d(in_channels=6, out_channels=6, kernel_size=(1, 1), padding=(0, 0))
+			self.conv3 = nn.Conv2d(in_channels=6, out_channels=6, kernel_size=(1, 1), padding=(0, 0))
+			self.conv4 = nn.Conv2d(in_channels=6, out_channels=1, kernel_size=(1, 1), padding=(0, 0))
 
 	def forward(self, x, vel_div, flags, normalizeInputChan, normalizeInputThrehsold):
-		self.setWallBcs(flags, vel_div)
 		# tfluids.FlagsToOccupancy() will create a [0, 1] grid out of the Manta flags : 1->0,2->1
-		x[3, :, :] = x[3, :, :] - 1
+		x[:, 3, :, :] = x[:, 3, :, :] - 1
 		# scale
-		if normalizeInputChan == 0:
-			scale_temp = x[:,:2,:,:].view(x.shape[0],-1)
-		elif normalizeInputChan == 1:
-			scale_temp = x[:, 2, :, :].view(x.shape[0], -1)
-		std = torch.std(scale_temp)
+		if normalizeInputChan == 0:  # pDiv
+			temp = x[:, 0, :, :].view(x.shape[0], -1)
+		elif normalizeInputChan == 1:  # UDiv
+			temp = x[:, 1:3, :, :].view(x.shape[0], -1)
+		std = torch.std(temp, dim=1)
 		scale = torch.clamp(std, normalizeInputThrehsold, float('inf'))
-		x[:,:3,:,:] /= scale
+		for ibatch in range(temp.shape[0]):
+			x[ibatch, :3, :, :] /= scale[ibatch]
+
 		# forward
-		x = F.relu(self.conv1(x))
-		x = F.relu(self.conv2(x))
-		x = F.relu(self.conv3(x))
-		x = F.relu(self.conv4(x))
-		p_pred = torch.squeeze(self.conv5(x), 1)
+		if self.modelType == 'default':
+			x = F.relu(self.conv1(x))
+			x = F.relu(self.conv2(x))
+			x = F.relu(self.conv3(x))
+			x = F.relu(self.conv4(x))
+			p_pred = torch.squeeze(self.conv5(x), 1)
+		if self.modelType == 'tog':
+			x = F.relu(self.conv1(x))
+			x = self.pool1(x)
+			x = F.relu(self.conv2(x))
+			x = F.relu(self.conv3(x))
+			x = F.relu(self.conv4(x))
+			x = F.relu(self.conv5(x))
+			x = F.relu(self.conv6(x))
+			p_pred = torch.squeeze(self.convTrans7(x), 1)
+		if self.modelType == 'yang':
+			x = F.relu(self.conv1(x))
+			x = F.relu(self.conv2(x))
+			x = F.relu(self.conv3(x))
+			p_pred = torch.squeeze(self.conv4(x), 1)
 		v_pred = self.correctVel(flags, vel_div, p_pred)
+
 		# reverse scale
-		p_pred *= scale
-		v_pred *= scale
-
-
+		for ibatch in range(temp.shape[0]):
+			p_pred[ibatch] *= scale[ibatch]
+			v_pred[ibatch] *= scale[ibatch]
+		# setWallBcs
+		v_pred = self.setWallBcs(flags, v_pred)
 		return p_pred, v_pred
 
 	def correctVel(self, flags, vel, pressure):
@@ -94,29 +128,31 @@ class PressureNet(nn.Module):
 	# mantaflow setWallBcs()
 	# set obstacle boundary conditions
 	# ! set no-stick wall boundary condition between ob/fl and ob/ob cells
-	def setWallBcs(flags, vel):
+	def setWallBcs(self, flags, vel):
 		vel_next = vel.clone()
-		vel_next[:,1:,0] = (((flags[:,1:]&1!=0)+(flags[:,1:]&2!=0))>0) * (flags[:,:-1]&2!=0) * 0.0 + ((flags[:,1:]&1==0) * (flags[:,1:]&2==0)+(flags[:,:-1]&2==0)>0) * vel_next[:,1:,0]
-		vel_next[:,1:,0] = (flags[:,1:]&2!=0)*(flags[:,:-1]&1!=0) * 0.0 + (((flags[:,1:]&2==0)+(flags[:,:-1]&1==0))>0) * vel_next[:,1:,0]
-		vel_next[1:,:,1] = (((flags[1:,:]&1!=0)+(flags[1:,:]&2!=0))>0) * (flags[:-1,:]&2!=0) * 0.0 + ((flags[1:,:]&1==0) * (flags[1:,:]&2==0)+(flags[:-1,:]&2==0)>0) * vel_next[1:,:,1]
-		vel_next[1:,:,1] = (flags[1:,:]&2!=0)*(flags[:-1,:]&1!=0) * 0.0 + (((flags[1:,:]&2==0)+(flags[:-1,:]&1==0))>0) * vel_next[1:,:,1]
-		vel_next[:,1:,1] = (flags[:,1:]&1!=0)*(flags[:,:-1]&64!=0) * 0.0 + (((flags[:,1:]&1==0)+(flags[:,:-1]&64==0))>0) * vel_next[:,1:,1]
-		vel_next[:,:-1,1] = (flags[:,:-1]&1!=0)*(flags[:,1:]&64!=0) * 0.0 + (((flags[:,:-1]&1==0)+(flags[:,1:]&64==0))>0) * vel_next[:,:-1,1]
-		vel_next[1:,:,0] = (flags[1:,:]&1!=0)*(flags[:-1,:]&64!=0) * 0.0 + (((flags[1:,:]&1==0)+(flags[:-1,:]&64==0))>0) * vel_next[1:,:,0]
-		vel_next[:-1,:,0] = (flags[:-1,:]&1!=0)*(flags[1:,:]&64!=0) * 0.0 + (((flags[:-1,:]&1==0)+(flags[1:,:]&64==0))>0) * vel_next[:-1,:,0]
+		vel_next[:,:,1:,0] = (((flags[:,:,1:]&1!=0)+(flags[:,:,1:]&2!=0))>0) * (flags[:,:,:-1]&2!=0) * 0.0 + ((flags[:,:,1:]&1==0) * (flags[:,:,1:]&2==0)+(flags[:,:,:-1]&2==0)>0) * vel_next[:,:,1:,0]
+		vel_next[:,:,1:,0] = (flags[:,:,1:]&2!=0)*(flags[:,:,:-1]&1!=0) * 0.0 + (((flags[:,:,1:]&2==0)+(flags[:,:,:-1]&1==0))>0) * vel_next[:,:,1:,0]
+		vel_next[:,1:,:,1] = (((flags[:,1:,:]&1!=0)+(flags[:,1:,:]&2!=0))>0) * (flags[:,:-1,:]&2!=0) * 0.0 + ((flags[:,1:,:]&1==0) * (flags[:,1:,:]&2==0)+(flags[:,:-1,:]&2==0)>0) * vel_next[:,1:,:,1]
+		vel_next[:,1:,:,1] = (flags[:,1:,:]&2!=0)*(flags[:,:-1,:]&1!=0) * 0.0 + (((flags[:,1:,:]&2==0)+(flags[:,:-1,:]&1==0))>0) * vel_next[:,1:,:,1]
+		vel_next[:,:,1:,1] = (flags[:,:,1:]&1!=0)*(flags[:,:,:-1]&64!=0) * 0.0 + (((flags[:,:,1:]&1==0)+(flags[:,:,:-1]&64==0))>0) * vel_next[:,:,1:,1]
+		vel_next[:,:,:-1,1] = (flags[:,:,:-1]&1!=0)*(flags[:,:,1:]&64!=0) * 0.0 + (((flags[:,:,:-1]&1==0)+(flags[:,:,1:]&64==0))>0) * vel_next[:,:,:-1,1]
+		vel_next[:,1:,:,0] = (flags[:,1:,:]&1!=0)*(flags[:,:-1,:]&64!=0) * 0.0 + (((flags[:,1:,:]&1==0)+(flags[:,:-1,:]&64==0))>0) * vel_next[:,1:,:,0]
+		vel_next[:,:-1,:,0] = (flags[:,:-1,:]&1!=0)*(flags[:,1:,:]&64!=0) * 0.0 + (((flags[:,:-1,:]&1==0)+(flags[:,1:,:]&64==0))>0) * vel_next[:,:-1,:,0]
 		return vel_next
 
 
 class LossFunc(nn.Module):
 	def __init__(self):
 		super(LossFunc, self).__init__()
+		self.pLoss = nn.MSELoss(reduction='sum')
+		self.vLoss = nn.MSELoss(reduction='sum')
 		self.divergenceOpVel = DivergenceOpVel()
+		self.divLoss = nn.MSELoss(reduction='sum')
 
 	def forward(self, lamda_p, lamda_v, lamda_div, th_p, th_v, th_netp, th_netv):
-		error_p = th_p - th_netp
-		error_v = th_v - th_netv
-		error_div = self.divergenceOpVel(th_netv)
-		return (lamda_p * torch.sum(error_p * error_p) + lamda_v * torch.sum(error_v * error_v) + lamda_div * torch.sum(error_div * error_div)) / th_p.shape[0]
+		v_div = self.divergenceOpVel(th_netv)
+		target_div = torch.zeros_like(v_div)
+		return lamda_p * self.pLoss(th_netp, th_p) + lamda_v * self.vLoss(th_netv, th_v) + lamda_div * self.divLoss(v_div, target_div)
 
 
 class DivergenceOpVel(nn.Module):
--- train0_2d4c_nolong.py
+++ train0_2d4c_nolong.py
@@ -3,132 +3,138 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 import FluidNet_2d4c_no_long
 from torch.utils.tensorboard import SummaryWriter
-import math
 import time
-
-time_zero = time.time()
-time_last = time_zero
-writer = SummaryWriter('runs')
-
-# gpu
-use_gpu = torch.cuda.is_available()
+import json
+import csv
 
 # config
-train_batch_size = 16
-test_batch_size = 16
-train_shuffle = True
-test_shuffle = False
-train_num_workers = 2
-test_num_workers = 2
+writer = SummaryWriter('runs')
 
-epoch = 0
-normalizeInputChan = torch.tensor(0) # 0:'UDiv' or 1:'pDiv' or 2:'div'
-# normalizeInputFunc = 'std'
-normalizeInputThrehsold = torch.tensor(0.00001)
+F_TIME = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+LAST_MODEL_PATH = './'+F_TIME+'-last_presnet_state_dict.pth'
+LAST_OPTIMIZER_PATH = './'+F_TIME+'-last_optimizer_state_dict.pth'
+OPTIMAL_MODEL_PATH = './'+F_TIME+'-optimal_presnet_state_dict.pth'
+TRAIN_CONF_PATH = './'+F_TIME+'-train_conf.json'
+TRAIN_LOG_PATH = './'+F_TIME+'-train_log.csv'
+with open(TRAIN_LOG_PATH, 'w') as logfile:
+	w = csv.writer(logfile)
+	w.writerow(['Epoch', 'Train_loss', 'Test_loss'])
+
+train_conf = {
+	'modelType': 'default',  # 'default' 'tog' 'yang'
+	'normalizeInputChan': 0,  # 0:'pDiv' or 1:'UDiv' or 2:'div' normalizeInputFunc = 'std'
+	'normalizeInputThreshold': 0.00001,
+	'maxEpochs': 60,  # 5000
+	'batch_size': 16,
+	'lamda_ndiv': 0.0,
+	'lamda_div': 0.6,
+	'lamda_p': 0.2,
+	'lamda_v': 0.2,
+	'last_epoch': -1,
+	'optimal_epoch': -1,
+	'optimal_avg_train_loss': float('inf'),
+	'optimal_avg_test_loss': float('inf')
+}
 
 # data
 trainset = FluidNet_2d4c_no_long.VPGDdataset(isTrainset=True)
-trainloader = DataLoader(trainset, batch_size=train_batch_size, shuffle=train_shuffle, num_workers=train_num_workers)
+trainloader = DataLoader(trainset, batch_size=train_conf['batch_size'], shuffle=True, num_workers=2)
 testset = FluidNet_2d4c_no_long.VPGDdataset(isTrainset=False)
-testloader = DataLoader(testset, batch_size=test_batch_size, shuffle=test_shuffle, num_workers=test_num_workers)
+testloader = DataLoader(testset, batch_size=16, shuffle=False, num_workers=2)
 
 # model
-presnet = FluidNet_2d4c_no_long.PressureNet()
-
-
-
-
-maxEpochs = 50 #5000
-optimal_epoch = 0
-optimal_test_loss = float('inf')
-
-
-lamda_ndiv = 0.0
-lamda_div = torch.tensor(1.0)
-lamda_p = torch.tensor(0.0)
-lamda_v = torch.tensor(0.0)
-PATH = './lamda_%.2f_%.2f_%.2f_%.2f_bs_%03d.pth' % (lamda_ndiv, lamda_div, lamda_p, lamda_v, train_batch_size)
-PATH_LAST = './lamda_%.2f_%.2f_%.2f_%.2f_bs_%03d_last.pth' % (lamda_ndiv, lamda_div, lamda_p, lamda_v, train_batch_size)
-
-dt = 0.1
-dx = 1/128
-gravity = torch.tensor([0, -6e-4])
-
-
-train_batch_total = math.ceil(len(trainset) / train_batch_size)
-test_batch_total = math.ceil(len(testset) / test_batch_size)
-
-
+presnet = FluidNet_2d4c_no_long.PressureNet(modelType=train_conf['modelType'])
+optimizer = optim.Adam(presnet.parameters())
 lossFunc = FluidNet_2d4c_no_long.LossFunc()
+
+use_gpu = torch.cuda.is_available()
 if use_gpu:
 	presnet = presnet.cuda()
 	lossFunc = lossFunc.cuda()
-optimizer = optim.Adam(presnet.parameters())
 
-while epoch < maxEpochs:
-	time_start = time.time()
-	print('Epoch: %02d  Time: %dm  Delta_time: %dm ...' % (epoch, (time_start-time_zero)//60, (time_start-time_last)//60))
-	time_last = time_start
-	# Train code
-	train_loss = 0.0
-	for data_batched in trainloader:
-		if use_gpu:
-			data_batched['th_net_input'] = data_batched['th_net_input'].cuda()
-			data_batched['th_vx'] = data_batched['th_vx'].cuda()
-			data_batched['th_gx'] = data_batched['th_gx'].cuda()
-			lamda_p = lamda_p.cuda()
-			lamda_v = lamda_v.cuda()
-			lamda_div = lamda_div.cuda()
-			data_batched['th_p'] = data_batched['th_p'].cuda()
-			data_batched['th_v'] = data_batched['th_v'].cuda()
-		optimizer.zero_grad()
-		pres, vel = presnet(data_batched['th_net_input'], data_batched['th_vx'], data_batched['th_gx'], normalizeInputChan=normalizeInputChan, normalizeInputThrehsold=normalizeInputThrehsold)
-		loss = lossFunc(lamda_p=lamda_p, lamda_v=lamda_v, lamda_div=lamda_div, th_p=data_batched['th_p'], th_v=data_batched['th_v'], th_netp=pres, th_netv=vel)
-		loss.backward()
-		optimizer.step()
-		if use_gpu:
-			loss = loss.cpu()
-		train_loss += loss.item()
-	train_loss /= train_batch_total
-	# test code
-	test_loss = 0.0
-	with torch.no_grad():
-		for data_batched in testloader:
+# init
+presnet.load_state_dict(torch.load('./20200229_005124-presnet.pth'))
+epoch = 1
+time_zero = time.time()
+if __name__ == '__main__':
+	while epoch < train_conf['maxEpochs']:
+		print('Epoch: %02d' % epoch)
+		time_start = time.time()
+
+		# Train code
+		train_loss = 0.0
+		for data_batched in trainloader:
 			if use_gpu:
 				data_batched['th_net_input'] = data_batched['th_net_input'].cuda()
 				data_batched['th_vx'] = data_batched['th_vx'].cuda()
 				data_batched['th_gx'] = data_batched['th_gx'].cuda()
-				lamda_p = lamda_p.cuda()
-				lamda_v = lamda_v.cuda()
-				lamda_div = lamda_div.cuda()
 				data_batched['th_p'] = data_batched['th_p'].cuda()
 				data_batched['th_v'] = data_batched['th_v'].cuda()
-			pres, vel = presnet(data_batched['th_net_input'], data_batched['th_vx'], data_batched['th_gx'], normalizeInputChan=normalizeInputChan, normalizeInputThrehsold=normalizeInputThrehsold)
-			loss = lossFunc(lamda_p=lamda_p, lamda_v=lamda_v, lamda_div=lamda_div, th_p=data_batched['th_p'], th_v=data_batched['th_v'], th_netp=pres, th_netv=vel)
+				lamda_p = torch.tensor(train_conf['lamda_p']).cuda()
+				lamda_v = torch.tensor(train_conf['lamda_v']).cuda()
+				lamda_div = torch.tensor(train_conf['lamda_div']).cuda()
+			optimizer.zero_grad()
+			pres, vel = presnet(data_batched['th_net_input'], data_batched['th_vx'], data_batched['th_gx'], normalizeInputChan=train_conf['normalizeInputChan'], normalizeInputThrehsold=train_conf['normalizeInputThreshold'])
+			loss = lossFunc(lamda_p=train_conf['lamda_p'], lamda_v=train_conf['lamda_v'], lamda_div=train_conf['lamda_div'], th_p=data_batched['th_p'], th_v=data_batched['th_v'], th_netp=pres, th_netv=vel)
+			loss.backward()
+			optimizer.step()
 			if use_gpu:
 				loss = loss.cpu()
-			test_loss += loss.item()
-		test_loss /= test_batch_total
-	# save model
-	if test_loss < optimal_test_loss:
+			train_loss += loss.item()
+		train_loss /= len(trainset)
+		# test code
+		test_loss = 0.0
+		presnet.eval()
+		with torch.no_grad():
+			for data_batched in testloader:
+				if use_gpu:
+					data_batched['th_net_input'] = data_batched['th_net_input'].cuda()
+					data_batched['th_vx'] = data_batched['th_vx'].cuda()
+					data_batched['th_gx'] = data_batched['th_gx'].cuda()
+					data_batched['th_p'] = data_batched['th_p'].cuda()
+					data_batched['th_v'] = data_batched['th_v'].cuda()
+				pres, vel = presnet(data_batched['th_net_input'], data_batched['th_vx'], data_batched['th_gx'], normalizeInputChan=train_conf['normalizeInputChan'], normalizeInputThrehsold=train_conf['normalizeInputThreshold'])
+				loss = lossFunc(lamda_p=train_conf['lamda_p'], lamda_v=train_conf['lamda_v'], lamda_div=train_conf['lamda_div'], th_p=data_batched['th_p'], th_v=data_batched['th_v'], th_netp=pres, th_netv=vel)
+				if use_gpu:
+					loss = loss.cpu()
+				test_loss += loss.item()
+			test_loss /= len(testset)
+		presnet.train()
+
+		# save train and test log
+		with open(TRAIN_LOG_PATH, 'a') as logfile:
+			w = csv.writer(logfile)
+			w.writerow([epoch, train_loss, test_loss])
+		# write visiualization data
+		writer.add_scalar('train loss', train_loss, epoch)
+		writer.add_scalar('test loss', test_loss, epoch)
+		for name, layer in presnet.named_parameters():
+			writer.add_histogram(name + '_grad', layer.grad.data, epoch)
+			writer.add_histogram(name + '_weight', layer.data, epoch)
+		# save the last training model and update train conf
 		if use_gpu:
 			presnet = presnet.cpu()
-		torch.save(presnet.state_dict(), PATH)
+		torch.save(presnet.state_dict(), LAST_MODEL_PATH)
+		torch.save(optimizer.state_dict(), LAST_OPTIMIZER_PATH)
 		if use_gpu:
 			presnet = presnet.cuda()
-		optimal_epoch = epoch
-		optimal_test_loss = test_loss
-	if epoch == maxEpochs:
-		if use_gpu:
-			presnet = presnet.cpu()
-		torch.save(presnet.state_dict(), PATH_LAST)
-	# visiualization
-	writer.add_scalar('train loss', train_loss, epoch)
-	writer.add_scalar('test loss', test_loss, epoch)
-	for name,layer in presnet.named_parameters():
-		writer.add_histogram(name + '_grad', layer.grad.data, epoch)
-		writer.add_histogram(name + '_weight', layer.data, epoch)
-	epoch += 1
+		with open(TRAIN_CONF_PATH, 'w') as f:
+			train_conf['last_epoch'] = epoch
+			json.dump(train_conf, f)
+		# save optimal model and update train conf
+		if test_loss < train_conf['optimal_avg_test_loss']:
+			if use_gpu:
+				presnet = presnet.cpu()
+			torch.save(presnet.state_dict(), OPTIMAL_MODEL_PATH)
+			if use_gpu:
+				presnet = presnet.cuda()
+			with open(TRAIN_CONF_PATH, 'w') as f:
+				train_conf['optimal_epoch'] = epoch
+				train_conf['optimal_avg_test_loss'] = test_loss
+				train_conf['optimal_avg_train_loss'] = train_loss
+				json.dump(train_conf, f)
 
-
+		time_end = time.time()
+		print('  Train_loss: %.4f  Test_loss: %.4f  Delta_time: %dm  Total_time: %dm' % (train_loss, test_loss, (time_end-time_start)//60, (time_end-time_zero)//60))
+		epoch += 1
 
